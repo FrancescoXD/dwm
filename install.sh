73e161366bc65c20c7f7fba050505e5ab2a29225
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-08-07 01:00:22 +0200

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
+# Copy fonts
+mkdir $HOME/.local/share/fonts
+cp -r fonts* $HOME/.local/share/fonts
+
+# Copy wallpapers
 mkdir $HOME/Wallpapers
 cp -r wallpapers/* $HOME/Wallpapers
+
+# Copy scripts
 cp -r .dwm $HOME/
 chmod +x $HOME/.dwm/autostart.sh
+
+# Compile dwm
+cd dwm
+make
+sudo make install
+make clean
+cd ..
