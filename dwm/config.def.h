9a216f0110667dd9c7e0d88a889eb7b5daa98349
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-12-30 22:14:01 +0100

    removed yellow background color on bar

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -19,7 +19,8 @@ static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	//[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel] = { col_gray4, col_gray1, col_cyan },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
