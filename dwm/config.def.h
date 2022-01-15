bb5c5d8c8e8d608d4fd43deed7230699a9e6b122
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2022-01-15 12:20:08 +0000

    added flameshot shortcut

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -76,12 +76,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", "-l", "20", "-g", "2", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "20", "-g", "2", NULL };
+static const char *flameshot[] = { "flameshot", "gui", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,			XK_p,	   spawn,	   {.v = flameshot } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
